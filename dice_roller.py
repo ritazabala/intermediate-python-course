e293c99c17ee5bbc3305e1fa4c9e46c2afb843b3
ritazabala <82459669+ritazabala@users.noreply.github.com>
2021-09-04 17:10:54 -0300

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -2,8 +2,8 @@ import random
 def main():
   dice_rolls = 2
   for i in range(0,dice_rolls):
-  	rollo = random.randint(1,6)
-	  print ( f'Has lanzado un { rollo } ' )
+	rollo = random.randint(1,6)
+	print ( f'Has lanzado un { rollo } ' )
 
 if __name__== "__main__":
   main()
