5a01316f31dec78b7a4d060a978e048975557304
ritazabala <ritazabala28@gmail.com>
2021-09-04 17:45:00 -0300

    modifique

--- dice_roller.py
+++ dice_roller.py
@@ -5,8 +5,12 @@ def main():
   dice_sum = 0
   for i in range(0,rolls):
     roll = random.randint(1,6)
-    dice_sum = dice_sum + roll
+    if roll == 1:
+       print(f'¡Has obtenido un {roll}! Fallo critico')
+    else:
     print(f'haz lanzado un {roll}')
+    dice_sum = dice_sum + roll
+    
   print ( f'Has obtenido un total de {dice_sum}' )
 
 if __name__== "__main__":
