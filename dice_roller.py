2cba10e2e5b7005d8cd561283dd5fc70af5f2ace
ritazabala <ritazabala28@gmail.com>
2021-09-04 18:07:20 -0300

    modifique otra linea

--- dice_roller.py
+++ dice_roller.py
@@ -1,20 +1,20 @@
 import random 
 
 def main():
-  rolls = int(input('¿Cuántos dados te gustaría tirar?'))
-  size  =  int ( input ( '¿Cuántos lados tienen los dados?' ))
-  dice_sum = 0
-  for i in range(0,rolls):
-    roll = random.randint(1,size)
-    if roll == 1:
+  dice_rolls  =  int ( input ( '¿Cuántos dados te gustaría tirar?' ))
+  dice_size  =  int ( input ( '¿Cuántos lados tienen los dados?' ))
+  dice_sum  =  0 
+  for i in range(0,dice_rolls):
+    rollo = random.randint(1,dice_size)
+    dice_sum  + =  rollo 
+    if rollo == 1:
        print(f'¡Has obtenido un {roll}! Fallo crítico')
-    elif roll == size:
+    elif rollo == dice_size:
        print(f'Has obtenido un {roll} !Éxito crítico!')
     else:
        print(f'haz lanzado un {roll}')
-    dice_sum = dice_sum + roll
-    
-  print ( f'Has obtenido un total de {dice_sum}' )
+        
+  print (f'Has obtenido un total de {dice_sum}')
 
 if __name__== "__main__":
   main()
