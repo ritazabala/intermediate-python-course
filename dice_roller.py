720cc6b76279735208c533687b3f444d4eddb878
ritazabala <82459669+ritazabala@users.noreply.github.com>
2021-09-04 17:27:26 -0300

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -2,9 +2,12 @@ import random
 
 def main():
   rolls = 2
+  dice_sum = 0
   for i in range(0,rolls):
     roll = random.randint(1,6)
+    dice_sum = dice_sum + roll
     print(f'haz lanzado un {roll}')
+  print ( f'Has obtenido un total de {dice_sum}' )
 
 if __name__== "__main__":
   main()
